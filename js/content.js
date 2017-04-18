5e6cea24e775d937f431ea2db03015bfc872c776
Matthew Horwood <matt.horwood@mayden.co.uk>
2017-04-18 16:16:20 +0100

    move hide css code to a function to be able to call on page load

--- js/content.js
+++ js/content.js
@@ -1,11 +1,17 @@
-localStorage.setItem('which', '');
+hideit(localStorage.getItem('which'));
 
+function hideit(hide){
+    var list = document.getElementsByClassName("taskHeader clickable label-success");
+    var arr = Array.prototype.slice.call( list )
+    arr.forEach(myFunction);
+    function myFunction(item, index) {
+        item.parentElement.style.display = hide;
+    }
+}
 
 window.chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
     if (msg.text === 'find-stories') {
-        var list = document.getElementsByClassName("taskHeader clickable label-success");
-        var arr = Array.prototype.slice.call( list )
         var which;
         if (localStorage.getItem('which') === 'none') {
             which_get = '';
@@ -14,10 +20,7 @@ window.chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse)
             which_get = 'none';
             which_set = 'none';
         }
-        arr.forEach(myFunction);
-        function myFunction(item, index) {
-            item.parentElement.style.display = which_get;
-        }
+        hideit(which_get);
         localStorage.setItem('which', which_set);
     } else if (msg.text === 'v3:find-stories') {
         var list = document.getElementsByClassName("label-success");
